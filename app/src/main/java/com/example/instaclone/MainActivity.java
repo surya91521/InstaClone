7418c56e283869eb69c8e025e2186ba432de49da
fang009 <surya91521@gmail.com>
2020-04-30 01:00:25 +0530

    Uplloading image work going on

--- app/src/main/java/com/example/instaclone/MainActivity.java
+++ app/src/main/java/com/example/instaclone/MainActivity.java
@@ -2,10 +2,14 @@
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
+import android.Manifest;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.MenuItem;
 
@@ -26,6 +30,11 @@ import com.google.firebase.auth.FirebaseAuth;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (Build.VERSION.SDK_INT >= 23) {
+            isStoragePermissionGranted();
+        }
+
 
         bottomNavigationView = findViewById(R.id.bottom_navigation);
         bottomNavigationView.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
@@ -79,4 +88,33 @@ import com.google.firebase.auth.FirebaseAuth;
                       return true;
                 }
             };
+
+
+      public  boolean isStoragePermissionGranted() {
+          if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+              if (checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                      == PackageManager.PERMISSION_GRANTED) {
+                  //   Log.v(TAG,"Permission is granted");
+                  return true;
+              } else {
+
+                  //  Log.v(TAG,"Permission is revoked");
+                  ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+                  return false;
+              }
+          }
+          else { //permission is automatically granted on sdk<23 upon installation
+              //  Log.v(TAG,"Permission is granted");
+              return true;
+          }
+      }
+
+      @Override
+      public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+          super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+          if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+              //   Log.v(TAG,"Permission: "+permissions[0]+ "was "+grantResults[0]);
+              //resume tasks needing this permission
+          }
+      }
 }
